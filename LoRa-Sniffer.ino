530bafd7889c8c525fa061b1d519e08414acaaaa
Daniel Petikian <danielpetikian2023@u.northwestern.edu>
2022-06-02 12:19:37 -0500

    lora sniffer

---
+++ LoRa-Sniffer.ino
@@ -0,0 +1,48 @@
+/* 
+  Check the new incoming messages, and print via serialin 115200 baud rate.
+  
+  by Aaron.Lee from HelTec AutoMation, ChengDu, China
+  成都惠利特自动化科技有限公司
+  www.heltec.cn
+  
+  this project also realess in GitHub:
+  https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
+*/
+
+#include "heltec.h"
+#define BAND    903900000  //you can set band here directly,e.g. 868E6,915E6
+#define SYNCWORD 0x34
+#define SF 7
+#define BW 125E3
+void setup() {
+    //WIFI Kit series V1 not support Vext control
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  LoRa.receive();
+  LoRa.setSpreadingFactor(SF);
+  LoRa.setSignalBandwidth(BW);
+  LoRa.setSyncWord(SYNCWORD);
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("\nReceived packet '");
+    // read packet
+    while (LoRa.available()) {
+      byte b = LoRa.read();
+      if (b < 16) {Serial.print("0");}
+      Serial.printf("%x ",b);
+    }
+    // print RSSI of packet
+    Serial.print("'\nRSSI ");
+    Serial.println(LoRa.packetRssi());
+    Serial.print("SNR ");
+    Serial.println(LoRa.packetSnr());
+    Serial.printf("Bandwidth %d\n", BW);
+    Serial.printf("Frequency %d\n", BAND);
+    Serial.printf("Spreading Factor %d\n", SF);
+  }
+  
+}
